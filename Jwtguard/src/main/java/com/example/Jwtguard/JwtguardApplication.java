b175582bda9e31f3fc3d1f14502ed7a6c9730d01
Emine Tas <eminetas126@gmail.com>
2025-03-10 22:10:54 +0300

    C1

---
+++ Jwtguard/src/main/java/com/example/Jwtguard/JwtguardApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jwtguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtguardApplication.class, args);
+	}
+
+}
